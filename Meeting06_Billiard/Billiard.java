38701b334389b79d70ad281e10cb64f5a7704cfc
calvinjoed <calvinjoedennedy@gmail.com>
2019-06-27 00:09:44 +0700

    To Do 5

---
+++ Meeting06_Billiard/Billiard.java
@@ -0,0 +1,145 @@
+//package Meeting06_Billiard;
+
+/*
+	Matfis pertemuan 6
+	Half-fledged billiard
+
+	TODO:
+	 1. Create billiard balls, use 8-ball rules.
+	 2. Assign one ball to be the hitter (preferably not colored white)
+	 3. Create guideline to help aiming the hitter (refer to #04 Clasher)
+	 4. Add collision for ball against wall and ball against balls.
+	 5. Create holes for the balls
+	 6. Add additional mechanics for the game and scoring system
+ */
+
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.JFrame;
+
+public class Billiard {
+	private JFrame frame;
+	private Vector destination;
+	private int frameHeight;
+
+	//The collections of walls to be drawn
+	private ArrayList<Wall> walls = new ArrayList<>();
+	private ArrayList<Ball> balls = new ArrayList<>();
+	private Ball hitter;
+	private boolean loose = true; // sets if the ball will have huge gaps between, or very tight
+	private boolean betaCollision = true; // uses a hand-made collision detection using arctan, sin, and cos
+
+	private Billiard() {
+		//configure the main canvas
+		frame = new JFrame("Billiard");
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		frame.setBackground(Color.WHITE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+		frame.setVisible(true);
+		frameHeight = frame.getHeight() - frame.getInsets().top;
+		createObjects();
+
+		destination = new Vector(hitter.getPositionX(), hitter.getPositionY());
+		DrawingArea drawingArea = new DrawingArea(frame.getWidth(), frameHeight, balls, walls, destination, hitter);
+		// Add mouse listeners
+		frame.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                super.mousePressed(e);
+                drawingArea.setPress(true);
+                destination.setX((double) e.getX());
+                destination.setY((double) e.getY());
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                super.mouseReleased(e);
+
+                double distanceX = e.getX() - hitter.getPositionX();
+                double distanceY = e.getY() - hitter.getPositionY();
+                double distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+
+                hitter.setVelocityX(-drawingArea.getTime() * distanceX / distance);
+                hitter.setVelocityY(-drawingArea.getTime() * distanceY / distance);
+
+                drawingArea.setPress(false);
+            }
+        });
+
+        frame.addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                super.mouseMoved(e);
+                destination.setX((double) e.getX());
+                destination.setY((double) e.getY());
+            }
+        });
+		
+		frame.add(drawingArea);
+
+		drawingArea.start();
+	}
+
+	private void createObjects() {
+		int wallWidth = (int) (frame.getWidth() * 0.9);
+		int wallHeight = (int) (frameHeight * 0.6);
+		int wallX = (int) (frame.getWidth() * 0.05);
+		int wallY = (int) (frameHeight * 0.2);
+
+		// vertical wall must be defined in clockwise direction
+		// horizontal wall must be defined in counter clockwise direction
+		walls.add(new Wall(wallWidth + wallX, wallY, wallX, wallY));	// top wall
+		walls.add(new Wall(wallX, wallHeight + wallY, wallX, wallY));	// left wall
+		walls.add(new Wall(wallWidth + wallX, wallY, wallWidth + wallX, wallHeight + wallY));	// bottom wall
+		walls.add(new Wall(wallX, wallHeight + wallY, wallX + wallWidth, wallHeight + wallY));	// right wall
+
+		// setup inital ball coordinates
+		// "Nyontek dosa" - BeefBurrito
+		double gap = 8;
+		int ballCounter = 0;
+		int[] order = {9,7,12,8,15,6,10,3,14,11,2,13,4,5};
+		if(loose) {
+			// with gap to mitigate bad collision detection
+			balls.add(new Ball(frame.getWidth()*2/3 - Ball.RADIUS - 1, frameHeight/2 - gap, randomColor(), 1, betaCollision)); // -1 is due to OCD kicks in
+			for(int i = 2; i <= 5; i++) {
+				for(int j = 1; j <= i; j++) {
+					double padUp = (i+1)*(Ball.RADIUS);
+					double padRight = (i-1)*(Ball.RADIUS+gap)*Math.sqrt(3);
+					Color color = randomColor();
+					if(order[ballCounter] == 8) color = Color.black;
+					balls.add(new Ball(frame.getWidth()*2/3 - Ball.RADIUS + padRight, frameHeight/2 - Ball.RADIUS - padUp + j*2.5*Ball.RADIUS, color, order[ballCounter], betaCollision));
+					ballCounter++;
+				}
+			}
+		} else {
+			// "perfect smooth edition" with bad collision detection
+			balls.add(new Ball(frame.getWidth()*2/3 - Ball.RADIUS-1, frameHeight/2, randomColor(), 1, betaCollision)); // -1 is due to OCD kicks in
+			for(int i = 2; i <= 5; i++) {
+				for(int j = 1; j <= i; j++) {
+					double padUp = (i-1)*Ball.RADIUS + Ball.RADIUS;
+					double padRight = (i-1)*Ball.RADIUS*Math.sqrt(3);
+					Color color = randomColor();
+					if(order[ballCounter] == 8) color = Color.black;
+					balls.add(new Ball(frame.getWidth()*2/3 - Ball.RADIUS + padRight, frameHeight/2 - Ball.RADIUS - padUp + j*2*Ball.RADIUS, color, order[ballCounter], betaCollision));
+					ballCounter++;
+				}
+			}
+		}
+		// Add hitter
+		hitter = new Ball(frame.getWidth()/3, frameHeight/2, new Color(0xdbdbdb), 0, betaCollision);
+		balls.add(hitter);
+	}
+	private Color randomColor() {
+		Random randomGenerator = new Random();
+		Color color = new Color(randomGenerator.nextInt(255), randomGenerator.nextInt(255), randomGenerator.nextInt(255));
+		return color.darker();
+	}
+	public static void main(String[] args) {
+		EventQueue.invokeLater(Billiard::new);
+	}
+}
---
+++ Meeting06_Billiard/DrawingArea.java
@@ -0,0 +1,163 @@
+
+//package Meeting06_Billiard;
+
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.geom.Line2D;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class DrawingArea extends JPanel {
+    private double time = 0;
+    private final static double TIME_INCREASE = 0.25;
+    private boolean press = false;
+    private boolean gameOver = false;
+    private int hits = 0;
+    private int height;
+    private int width;
+    private ArrayList<Ball> balls;
+    private ArrayList<Wall> walls;
+    private ArrayList<Hole> holes;
+    private Line2D guideline;
+    private Vector destination;
+    private Thread animator;
+    private BufferedImage drawingArea;
+    private Ball hitter;
+
+    public DrawingArea(int width, int height, ArrayList<Ball> balls, ArrayList<Wall> walls, Vector destination, Ball hitter) {
+        super(null);
+        this.height = height;
+        this.width = width;
+        setBounds(0, 0, width, height);
+        this.balls = balls;
+        this.walls = walls;
+        this.hitter = hitter;
+        this.destination = destination;
+        animator = new Thread(this::eventLoop);
+        guideline = new Line2D.Double(hitter.getPositionX(), hitter.getPositionY(), destination.getX(), destination.getY());
+        holes = new ArrayList<Hole>();
+        holes.add(new Hole(walls.get(0).getX1()-25, walls.get(0).getY1()+25, 50));
+        holes.add(new Hole(walls.get(0).getX2()+25, walls.get(0).getY1()+25, 50));
+
+        holes.add(new Hole(walls.get(1).getX2()+25, walls.get(1).getY1()-25, 50));
+        holes.add(new Hole(walls.get(2).getX1()-25, walls.get(2).getY2()-25, 50));
+        
+    }
+
+    public void start() {
+        animator.start();
+    }
+
+    public boolean isPressed() {
+        return press;
+    }
+
+    public void setPress(boolean press) {
+        this.press = press;
+        if (!press) {
+            time = 0;
+            if(!gameOver) hits++;
+        }
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    private void eventLoop() {
+        drawingArea = (BufferedImage) createImage(width, height);
+        while (true) {
+            update();
+            render();
+            printScreen();
+            if(gameOver) break;
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+                break;
+            }
+        }
+    }
+
+    private void update()
+    {
+        if(press && time < 10) {
+            time += TIME_INCREASE;
+        }
+        for(Ball b : balls)
+        {
+            b.move();
+            b.wallCollide(walls);
+            b.ballCollide(balls);
+        }
+        for(Ball b : balls) {
+            if(b.holeCollision(holes) && !hitter.equals(b)) {
+                if(b.getBallNumber() == 8 && balls.size() > 3)
+                    gameOver = true;
+                balls.remove(b);
+                break;
+            }
+        }
+        guideline.setLine(hitter.getPositionX(), hitter.getPositionY(), destination.getX(), destination.getY());
+    }
+
+    private void render()
+    {
+        if(drawingArea != null)
+        {
+            //get graphics of the image where coordinate and function will be drawn
+            Graphics g = drawingArea.getGraphics();
+
+            //clear screen
+            g.setColor(Color.white);
+            g.fillRect(0, 0, getWidth(), getHeight());
+
+            // set text for power
+            g.setColor(Color.black);
+            g.setFont(new Font("Consolas", Font.PLAIN, 24));
+            g.drawString("Power: " + Integer.toString((int)time), 30, 30);
+            g.drawString("Hits: " + Integer.toString(hits), 30, 60);
+            for(Ball b : balls) {
+                b.draw(g);
+            }
+
+            for(Wall w : walls) {
+                w.draw(g);
+            }
+            for(Hole h : holes) {
+                h.draw(g);
+            }
+            if (guideline != null) {
+                g.setColor(Color.red);
+                g.drawLine((int) guideline.getX1(), (int) guideline.getY1(), (int) guideline.getX2(), (int) guideline.getY2() - (int)Ball.RADIUS);
+            }
+            g.setFont(new Font("Consolas", Font.PLAIN, 120));
+            if(gameOver)
+                g.drawString("Game Over!", width/2 - 240, height/2);
+        }
+    }
+
+    private void printScreen()
+    {
+        try
+        {
+            Graphics g = getGraphics();
+            if(drawingArea != null && g != null)
+            {
+                g.drawImage(drawingArea, 0, 0, null);
+            }
+            // Sync the display on some systems.
+            // (on Linux, this fixes event queue problems)
+            Toolkit.getDefaultToolkit().sync();
+            g.dispose();
+        }
+        catch(Exception ex)
+        {
+            System.out.println("Graphics error: " + ex);
+        }
+    }
+}
